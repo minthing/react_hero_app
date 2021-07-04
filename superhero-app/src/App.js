9c2f0d09313ab7ca082e161701b5ad880cac5cee
minthing <minjeongandk@gmail.com>
2021-07-04 20:34:38 +0900

    test

--- superhero-app/src/App.js
+++ superhero-app/src/App.js
@@ -1,11 +1,9 @@
 import './App.css';
-import Potato from './Potato';
 
 function Hello() {
   return ( // 컴포넌트는 html을 반환하는 함수이다
     <div>
       <h1>hello hero</h1>
-      <Potato/>
     </div>
   );
 }
