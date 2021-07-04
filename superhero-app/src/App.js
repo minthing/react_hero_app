abb6d1f138a929ea6fc3cf09c389f0898a1f2a91
minthing <minjeongandk@gmail.com>
2021-07-04 20:50:31 +0900

    비구조화 할당

--- superhero-app/src/App.js
+++ superhero-app/src/App.js
@@ -1,11 +1,21 @@
 import './App.css';
 
-function Hello() {
+function Hello({world}){
+  console.log(world)
+  return (
+    <div>
+      I, {world}!
+    </div>
+  )
+}
+
+function App() {
   return ( // 컴포넌트는 html을 반환하는 함수이다
     <div>
-      <h1>hello hero</h1>
+      <h1>hello World!</h1>
+      <Hello world="dio" />
     </div>
   );
 }
 
-export default Hello;
+export default App;
