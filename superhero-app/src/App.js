6be5ca4556c4878beea384d28ddfdfb53ab0add2
minthing <minjeongandk@gmail.com>
2021-10-14 19:41:29 +0900

    3-0 : state

--- superhero-app/src/App.js
+++ superhero-app/src/App.js
@@ -1,63 +1,32 @@
 import './App.css';
-import propTypes from 'prop-types';
+import React from "react";
+// import propTypes from 'prop-types';
 
-function Heros({name, img, birthday}){
-  return (
-    <div>
-      <h2>hero name: {name}!</h2>
-      <h4>hero's birthday : {birthday}</h4>
-      <img src={img} alt={name} />
+class App extends React.Component{
+  // function이 아니기 때문에 return 안 씀. 대신 render 사용
+  // 자동적으로 class를 실행함
+  // class는 state를 가지고 있다 -> object임
+  // https://reactjs.org/docs/react-component.html
+
+  state = {
+    count : 0
+  }
+
+  add = () => {
+    console.log("add")
+  }
+  minus = () => {
+    console.log("minus")
+  }
+  render(h) {
+    // vue의 data 같은데...
+    // this.add() : 즉시실행
+    return <div>
+      <h1>The number is {this.state.count}</h1>
+      <button onClick={this.add}>plus</button>
+      <button onClick={this.minus}>minus</button>
     </div>
-  )
-}
-
-
-// propTypes => 데이터 타입이 유효한지를 확인
-
-Heros.propTypes = {
-  name: propTypes.string.isRequired,
-  img: propTypes.string.isRequired,
-  birthday : propTypes.string.isRequired
-};
-
-//unique key 없으면 에러남
-const heroList = [{
-  birthday:"Fab 29th",
-  id:1,
-  name:"superman",
-  img:"https://upload.wikimedia.org/wikipedia/en/3/35/Supermanflying.png"
-},
-{
-  birthday:"March 30th",
-  id:2,
-  name:"batman",
-  img:"https://upload.wikimedia.org/wikipedia/en/c/c7/Batman_Infobox.jpg"
-}]
-
-// const friends = ["a", "b", "c"];
-
-// friends.map(current => {
-//   console.log(current);
-//   return current + "✨";
-// })
-
-function renderHero(hero){
-  console.log(hero);
-  return <Heros key={hero.id} name={hero.name} img={hero.img} birthday={hero.birthday}/>
-}
-
-function App() {
-  return ( // 컴포넌트는 html을 반환하는 함수이다
-    <div>
-      {/* <Heros name="batman" list={[1,2,3,4,5]} /> */}
-      {/* <Heros name="superman" />
-      <Heros name="batman" />
-      <Heros name="flash" />
-      <Heros name="green lantern" /> */}
-    {heroList.map(renderHero)}
-    {/* {heroList.map(hero => {return <Heros name={hero.name} img={hero.img}/>})} */}
-    </div>
-  );
+  }
 }
 
 export default App;
