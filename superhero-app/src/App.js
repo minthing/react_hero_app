9e81577b0387112d250dfa809c8b195ccf9e8c16
minthing <minjeongandk@gmail.com>
2021-10-20 05:06:17 +0900

    axios

--- superhero-app/src/App.js
+++ superhero-app/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React from "react";
+import axios from "axios";
 // import propTypes from 'prop-types';
 
 class App extends React.Component{
@@ -8,10 +9,15 @@ class App extends React.Component{
     movies : []
   }
 
+  getMovies = async () => {
+    const movies = await axios.get("https://yts-proxy.now.sh/list_movies.json")
+  }
+
   componentDidMount(){
     setTimeout(()=>{
       this.setState({isLoading:false})
     }, 6000)
+    this.getMovies();
   }
 
 
